eaa802a7247499a592b670f425c91de079aef1d1
Jordan Weyer <jordantweyer@gmail.com>
2016-12-05 17:15:16 -0600

    Updated

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,34 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by Jordan Weyer on 12/5/16.
+//  Copyright © 2016 Jordan Weyer. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+    
+}
---
+++ pokedex3/Pokemon.swift
@@ -0,0 +1,30 @@
+//
+//  Pokemon.swift
+//  pokedex3
+//
+//  Created by Jordan Weyer on 12/5/16.
+//  Copyright © 2016 Jordan Weyer. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        
+        return _name
+    }
+    var pokedexId: Int {
+        
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
